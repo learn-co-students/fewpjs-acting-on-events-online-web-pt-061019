64f3f8c468e9b89ed54923695d239e51f5b9a2fa
ZumbaIdol <david.a.foote@gmail.com>
2020-04-23 15:50:32 -0500

    Done.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
    
-    if (left > 0) {
+    if (left > 0 and left < 360) {
       dodger.style.left = `${left + 1}px`;
     }
   }
